0fcd34d038693c80a0f6958983927b502f009815
Menigoz-Ewan <ewan.menigoz@edu.univ-fcomte.fr>
2026-03-06 15:14:32 +0100

    push

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -2,5 +2,19 @@ import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
 
 
-const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+const pb = new PocketBase('https://agence.menigoz.optimiseus.fr:443') as TypedPocketBase;
 export default pb;
+
+export async function getAgents() {
+    try {
+        let agents = await pb.collection("Agent").getFullList();
+        return agents;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des agents :", error);
+        return [];
+    }
+}
+
+export async function setFavori(house: any) {
+    await pb.collection('Maison').update(house.id, { favori: !house.favori });
+}
